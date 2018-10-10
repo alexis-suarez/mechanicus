2397b4395a8ee88643fa8e38595bdb5085848fbe
alexissuarez <alexissuarezllamas@outlook.com>
2018-10-10 18:40:13 -0500

    client table view and register modal

--- frontend/src/app/components/pages/client/client-view/client-view.component.ts
+++ frontend/src/app/components/pages/client/client-view/client-view.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientViewComponent implements OnInit {
 
+  private list: any[] = [
+    {
+      name: 'Alexis Suárez Llamas',
+      rfc: 'SULA950925',
+      phone: '3171027849',
+      email: 'alexis.suarez@ejemplo.com'
+    },
+    {
+      name: 'Armando Suárez Avalos',
+      rfc: 'SULA950925',
+      phone: '3814830',
+      email: 'armando.suarez@ejemplo.com'
+    },
+    {
+      name: 'Alan Suárez Llamas',
+      rfc: 'SULA950925',
+      phone: '',
+      email: 'alan.suarez@ejemplo.com'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public getList(): any {
+    return this.list;
+  }
+
+  public newClient(): void {
+    
   }
 
 }
