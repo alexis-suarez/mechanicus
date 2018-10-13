e1d4965184c98e9fb900eebd65ce539009a07ed9
alexissuarez <alexissuarezllamas@outlook.com>
2018-10-13 12:59:54 -0500

    adding service route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { ClientViewComponent } from './components/pages/client/client-view/client-view.component';
+import { ServiceViewComponent } from './components/pages/service/service-view/service-view.component';
 
 const app_routes: Routes = [
     { path: 'home', component: HomeComponent },
 
     // Client Routes
     { path: 'client-view', component: ClientViewComponent},
+
+    // Service Routes
+    { path: 'service-view', component: ServiceViewComponent},
 
     // Default Route
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
