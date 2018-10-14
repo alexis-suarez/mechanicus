8950fbd9a057481ebbbd71611019f6b3c1bc690e
alexissuarez <alexissuarezllamas@outlook.com>
2018-10-13 23:01:25 -0500

    adding login to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,25 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { ClientViewComponent } from './components/pages/client/client-view/client-view.component';
 import { ServiceViewComponent } from './components/pages/service/service-view/service-view.component';
 import { EmployeeViewComponent } from './components/pages/employee/employee-view/employee-view.component';
 
 const app_routes: Routes = [
+    // Home Route
     { path: 'home', component: HomeComponent },
 
-    // Client Routes
+    // Login Route
+    { path: 'login', component: LoginComponent },
+
+    // Client Route
     { path: 'client-view', component: ClientViewComponent},
 
-    // Service Routes
+    // Service Route
     { path: 'service-view', component: ServiceViewComponent},
 
-    // Employee Routes
+    // Employee Route
     { path: 'employee-view', component: EmployeeViewComponent},
 
     // Default Route
