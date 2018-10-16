1dcc622a5780b5058a3eb3ad7d47daaf0b20e57f
alexissuarez <alexissuarezllamas@outlook.com>
2018-10-15 21:21:00 -0500

    fixing speed background

--- frontend/src/assets/js/app.js
+++ frontend/src/assets/js/app.js
@@ -69,7 +69,7 @@ particlesJS('particles-js',
     },
     "move": {
       "enable": true,
-      "speed": 8,
+      "speed": 3,
       "direction": "none",
       "random": false,
       "straight": false,
