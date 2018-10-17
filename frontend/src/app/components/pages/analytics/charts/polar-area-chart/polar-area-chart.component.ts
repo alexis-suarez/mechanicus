d7695c1d7225f7e97096311cd183f2089a362e62
alexissuarez <alexissuarezllamas@outlook.com>
2018-10-16 22:19:28 -0500

    creating charts html y typescript

--- frontend/src/app/components/pages/analytics/charts/polar-area-chart/polar-area-chart.component.ts
+++ frontend/src/app/components/pages/analytics/charts/polar-area-chart/polar-area-chart.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-polar-area-chart',
   templateUrl: './polar-area-chart.component.html',
   styleUrls: ['./polar-area-chart.component.css']
 })
-export class PolarAreaChartComponent implements OnInit {
+export class PolarAreaChartComponent {
+    // PolarArea
+  public polarAreaChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
+  public polarAreaChartData: number[] = [300, 500, 100, 40, 120];
+  public polarAreaLegend = true;
 
-  constructor() { }
+  public polarAreaChartType = 'polarArea';
 
-  ngOnInit() {
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
   }
 
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 }
--- frontend/src/app/components/pages/analytics/charts/radar-chart/radar-chart.component.ts
+++ frontend/src/app/components/pages/analytics/charts/radar-chart/radar-chart.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-radar-chart',
   templateUrl: './radar-chart.component.html',
   styleUrls: ['./radar-chart.component.css']
 })
-export class RadarChartComponent implements OnInit {
+export class RadarChartComponent {
+  // Radar
+  public radarChartLabels: string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
 
-  constructor() { }
+  public radarChartData: any = [
+    {data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B'}
+  ];
+  public radarChartType = 'radar';
 
-  ngOnInit() {
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
   }
 
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 }
